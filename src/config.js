7bf7d96a95ca06e775ccd843e22759021e25ba5a
Mprakash2002 <omjyotiprakash4@gmail.com>
2026-05-05 14:55:17 +0530

    removed localhost

---
+++ src/config.js
@@ -0,0 +1,3 @@
+const API_URL = "https://food-delivery-g3c7.onrender.com";
+
+export default API_URL;
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const storeContext =  createContext(null)
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({})
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-g3c7.onrender.com"
   const [token, setToken] = useState("")
   const [food_list, setFoodList] = useState([])
 
